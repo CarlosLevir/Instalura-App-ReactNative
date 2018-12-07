8062c47fde1de00beee44a2922662360c39ad2d8
Carlos Levir Freire de Souza <carlos.levir@secrelnet.local>
2018-12-07 16:38:50 -0300

    Build of Initial Feed

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+import image from './assets/image.jpg';
+import { ScrollView, FlatList } from 'react-native-gesture-handler';
 
 export default class App extends React.Component {
   render() {
+    const photos = [{id: 1, user: 'Levir'}, {id: 2, user: 'Yara'}, {id: 3, user: 'Henrique'},]
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+
+      <FlatList
+      keyExtractor={item => item.id.toString()}
+      data={photos}
+      renderItem={ (item) => 
+        <View>
+          <Text>{item.user}</Text>
+          <Image style={styles.image} source={image} />  
+        </View>
+        }
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
   },
+  image: {
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').width,
+  }
 });
